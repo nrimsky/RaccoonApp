97279d3a83cb4226a2fbdd2d02d6877241c1f9db
nrimsky <ninarimsky@gmail.com>
2022-07-14 18:32:03 +0100

    Initial Commit

---
+++ RaccoonApp/RaccoonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaccoonAppApp.swift
+//  RaccoonApp
+//
+//  Created by Nina Rimsky on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RaccoonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
